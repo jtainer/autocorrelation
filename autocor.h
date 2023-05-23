352ca78d8005ed43ef2c8f6ffe6741e6391df414
jtain <jtain@debian>
2023-05-23 03:13:36 -0400

    Added header

---
+++ autocor.h
@@ -0,0 +1,30 @@
+// 
+// Fast autocorrelation using Wiener-Khinchim theorem
+//
+// 2023, Jonathan Tainer
+// 
+// Introduction: Autocorrelation is the measure of how much a signal correlates
+// with a delayed copy of itself. This is useful for finding repeating patterns
+// or identifying the fundamental frequency of a noisy signal.
+//
+// Algorithm: To calculate the autocorrelation R(t) of a signal x(t),
+// 1. Take the fourier transform: X(f) = FFT(x(t)).
+// 2. Multiply the frequency-domain signal by its complex conjugate (flip the
+// sign of the imaginary component): S(f) = X(f) * X`(f).
+// 3. Take the inverse fourier transform of the product: R(T) = IFFT(S(f)).
+//
+
+#ifndef AUTOCOR_H
+#define AUTOCOR_H
+
+#include <complex.h>
+
+// Calculates autocorrelation of sig and stores result in cor.
+// Number of elements n must be a power of 2.
+void autocor(float* sig, float* cor, unsigned int n);
+
+// Calculates autocorrelation of sig in-place, overwriting the signal with the
+// resulting autocorrelation values.
+void autocor_inpl(float complex* sig, unsigned int n);
+
+#endif
